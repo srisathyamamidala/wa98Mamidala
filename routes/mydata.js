689b1c90d5409a7bf835bc254ca5bb7cfb6f7b0d
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-03-24 17:48:13 -0500

    modified files

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var abcd=0;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var sat=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  sat = sat+1;
+  res.send('User accesses are:' + sat);
 });
 
 module.exports = router;

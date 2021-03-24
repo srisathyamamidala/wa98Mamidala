e61685e533f4404efe9f8ff0f07d47020e12ca25
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-03-24 18:20:07 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    let someNumber=Math.floor(Math.random()*100)
+  res.render('index', { result:'log applied to'+someNumber+'is'+Math.log(someNumber)});
+});
+
+module.exports = router;

98b2ec85529110a0ccc19a459a2226b9de839153
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-03-24 19:33:57 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     let someNumber=Math.floor(Math.random()*100)
-  res.render('index', { result:'log applied to'+someNumber+'is'+Math.log(someNumber)});
+  res.render('compute', { result:'log applied to '+someNumber+' is '+Math.log(someNumber)});
 });
 
 module.exports = router;
